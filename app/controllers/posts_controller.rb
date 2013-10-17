336a8cea17ee4404412016b8f938551c7b17702e
mannyflugo <mannyflugo1@gmail.com>
2013-10-17 02:26:03 -0700

    fixed problem where couldn't create posts- had to add code to build post by specific user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,7 +14,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(params[:post])
+    @post = current_user.posts.build(params[:post])
     authorize! :create, @post, message: "You need to be signed up to do that."
     if @post.save
        flash[:notice] = "Post was saved."
